16dad6f34ec3db38022df163dfaae0024dc6e313
pvdrh <duyvpvnua@gmail.com>
2021-01-27 09:36:17 +0700

    init

--- src/router/index.js
+++ src/router/index.js
@@ -4,41 +4,34 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    redirect: '/login'
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: () => import('../components/admin/Admin')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../components/auth/Login')
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../components/auth/Register')
-  },
-  // {
-  //   path: '/forgot-password',
-  //   name: 'Forgot',
-  //   component: () => import('../components/Buoi5/auth/ForgotPassword')
-  // },
-  // {
-  //   path: '/logout',
-  //   redirect: 'login'
-  // }
+const routes = [{
+        path: '/',
+        redirect: '/login'
+    },
+    {
+        path: '/admin',
+        name: 'Admin',
+        component: () =>
+            import ('../components/admin/Admin')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () =>
+            import ('../components/auth/Login')
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () =>
+            import ('../components/auth/Register')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
